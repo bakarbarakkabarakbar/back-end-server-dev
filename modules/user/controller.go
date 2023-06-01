eafa4bfa23636ba1f2d522907036d72296c19bb8
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 23:23:07 +0700

    rebase: add create user controller

--- modules/user/controller.go
+++ modules/user/controller.go
@@ -34,6 +34,28 @@ func (ctrl Controller) CreateUser(req UserParam) (any, error) {
 	return res, nil
 }
 
+func (ctrl Controller) CreateUser(req UserParam) (any, error) {
+
+	user, err := ctrl.uc.CreateUser(req)
+	if err != nil {
+		return SuccessCreate{}, err
+	}
+	res := SuccessCreate{
+		ResponseMeta: dto.ResponseMeta{
+			Success:      true,
+			MessageTitle: "Success create user",
+			Message:      "Success Register",
+			ResponseTime: "",
+		},
+		Data: UserParam{
+			Name:     user.Name,
+			Email:    user.Email,
+			Password: user.Password,
+		},
+	}
+	return res, nil
+}
+
 func (ctrl Controller) GetUserById(id uint) (FindUser, error) {
 	var res FindUser
 	var user, err = ctrl.uc.GetUserById(id)
