268f82c1f6dd3f6bd8060207b22b7d9e66bdb6ff
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 23:22:03 +0700

    feat: add success create dto

--- modules/user/dto.go
+++ modules/user/dto.go
@@ -1,5 +1,7 @@
 package user
 
+import "github.com/dibimbing-satkom-indo/onion-architecture-go/dto"
+
 type Payload struct {
 	ID int
 }
@@ -8,4 +10,9 @@ type UserParam struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type SuccessCreate struct {
+	dto.ResponseMeta
+	Data UserParam `json:"data"`
 }
