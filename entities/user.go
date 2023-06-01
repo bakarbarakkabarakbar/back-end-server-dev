a08289c8a46b289581fb6ec228fe2c62b6bb035e
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 23:02:25 +0700

    build: add user struct

--- entities/user.go
+++ entities/user.go
@@ -1,8 +1,12 @@
 package entities
 
-// struktur data suatu table
+import "time"
+
 type User struct {
-	ID   int
-	Name string
-	Age  int
+	Id        uint   `gorm:"primary_key"`
+	Name      string `gorm:"column:name"`
+	Email     string `gorm:"column:email"`
+	Password  string `gorm:"column:password"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
