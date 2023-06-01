51402390fae950092c4800fdd20ef799d55daa7e
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 23:19:19 +0700

    feat: add user router

--- modules/user/router.go
+++ modules/user/router.go
@@ -1,6 +1,10 @@
 package user
 
-import "github.com/dibimbing-satkom-indo/onion-architecture-go/dto"
+import (
+	"github.com/dibimbing-satkom-indo/onion-architecture-go/dto"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type Router struct {
 	rq RequestHandlerInterface
@@ -11,5 +15,22 @@ func NewRouter() Router {
 }
 
 func (r Router) Route(request dto.Request) {
+
+}
+
+func (ru Router) Router(router *gin.Engine) {
+	var basePath = "/"
+	var basePathGroup = router.Group(basePath)
+	basePathGroup.GET("/ping",
+		func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+
+	var userPath = "/user"
+	var userPathGroup = router.Group(userPath)
+	userPathGroup.POST("/register", ru.rh.CreateUser)
+	userPathGroup.GET("/:id", ru.rh.GetUsedById)
 
 }
