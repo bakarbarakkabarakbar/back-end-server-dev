c51f12396a4b6a6276cd746b8ef9462f1de4c56b
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-01 23:23:07 +0700

    feat add create user controller

--- modules/user/controller.go
+++ modules/user/controller.go
@@ -11,3 +11,25 @@ type Controller struct {
 type ControllerInterface interface {
 	GetUserByID(payload Payload) dto.Response
 }
+
+func (ctrl Controller) CreateUser(req UserParam) (any, error) {
+
+	user, err := ctrl.uc.CreateUser(req)
+	if err != nil {
+		return SuccessCreate{}, err
+	}
+	res := SuccessCreate{
+		ResponseMeta: dto.ResponseMeta{
+			Success:      true,
+			MessageTitle: "Success create user",
+			Message:      "Success Register",
+			ResponseTime: "",
+		},
+		Data: UserParam{
+			Name:     user.Name,
+			Email:    user.Email,
+			Password: user.Password,
+		},
+	}
+	return res, nil
+}
