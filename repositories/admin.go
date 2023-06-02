9c404ffbf9e22e08b3f9ec959784ebf197449309
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-02 16:27:06 +0700

    feat: add create customer func

--- repositories/admin.go
+++ repositories/admin.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"github.com/dibimbing-satkom-indo/onion-architecture-go/entities"
 	"gorm.io/gorm"
 )
 
@@ -12,4 +13,9 @@ func NewAccountRepo(dbCrud *gorm.DB) AdminRepo {
 	return AdminRepo{
 		db: dbCrud,
 	}
+}
+
+func (ar AdminRepo) CreateCustomer(customer *entities.User) (*entities.User, error) {
+	err := ar.db.Model(&entities.User{}).Create(customer).Error
+	return customer, err
 }
