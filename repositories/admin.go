8a9866e11ec7dc1c2e08c5450b326c624fcda5e5
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-02 16:27:27 +0700

    feat: add remove customer by id func

--- repositories/admin.go
+++ repositories/admin.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"fmt"
 	"github.com/dibimbing-satkom-indo/onion-architecture-go/entities"
 	"gorm.io/gorm"
 )
@@ -18,4 +19,14 @@ func NewAccountRepo(dbCrud *gorm.DB) AdminRepo {
 func (ar AdminRepo) CreateCustomer(customer *entities.User) (*entities.User, error) {
 	err := ar.db.Model(&entities.User{}).Create(customer).Error
 	return customer, err
+}
+
+func (ar AdminRepo) RemoveCustomerById(id uint) (entities.User, error) {
+	var customer = entities.User{Id: id}
+	var err = ar.db.Delete(&customer).Error
+	if err != nil {
+		fmt.Println("Error RemoveCustomerById", err)
+		return customer, err
+	}
+	return customer, nil
 }
