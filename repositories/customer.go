c9803d01f4cec3cf569160040009062d6e56fb43
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-02 16:18:36 +0700

    fix: adjust customer interface to getcustomerbyid and byemail

--- repositories/customer.go
+++ repositories/customer.go
@@ -17,37 +17,25 @@ func NewCustomerRepo(dbCrud *gorm.DB) CustomerRepo {
 }
 
 type CustomerRepoInterface interface {
-	GetByID(id int) []entities.User
-	GetCustomers(user *entities.User) ([]entities.User, error)
 	GetCustomerById(id uint) (entities.User, error)
-	CreateCustomer(user *entities.User) (*entities.User, error)
-}
-
-func (cr CustomerRepo) GetByID(id int) []entities.User {
-	// implementasi query get customers by id
-	return []entities.User{}
-}
-
-func (cr CustomerRepo) CreateCustomer(user *entities.User) (*entities.User, error) {
-	err := cr.db.Model(&entities.User{}).Create(user).Error
-	return user, err
-}
-
-func (cr CustomerRepo) GetCustomers(user *entities.User) ([]entities.User, error) {
-	var users = make([]entities.User, 0)
-	var err = cr.db.Find(&users).Error
-	if err != nil {
-		fmt.Println("error GetCustomers")
-		return []entities.User{}, err
-	}
-	return users, nil
+	GetCustomerByEmail(email string) (entities.User, error)
 }
 
 func (cr CustomerRepo) GetCustomerById(id uint) (entities.User, error) {
 	var user entities.User
 	var err = cr.db.First(&user, "id", id).Error
 	if err != nil {
-		fmt.Println("error GetCustomers", err)
+		fmt.Println("error GetCustomersById", err)
+		return user, err
+	}
+	return user, nil
+}
+
+func (cr CustomerRepo) GetCustomerByEmail(email string) (entities.User, error) {
+	var user entities.User
+	var err = cr.db.First(&user, "Email", email).Error
+	if err != nil {
+		fmt.Println("error GetCustomersByEmail", err)
 		return user, err
 	}
 	return user, nil
