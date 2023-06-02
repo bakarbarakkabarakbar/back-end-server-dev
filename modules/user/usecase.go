d6489e45434c79cb067f2a211177bd11d84725e1
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-02 11:36:35 +0700

    feat: add create user func and insert to interface

--- modules/user/usecase.go
+++ modules/user/usecase.go
@@ -9,11 +9,30 @@ type UseCase struct {
 	userRepo repositories.UserRepoInterface
 }
 
-type UsecaseInterface interface {
+type UseCaseInterface interface {
+	CreateUser(user UserParam) (entities.User, error)
 	GetUserByID(payload Payload) []entities.User
 }
 
 func (uc UseCase) GetUserById(id uint) (entities.User, error) {
 	var user, err = uc.userRepo.GetUserById(id)
 	return user, err
+}
+
+func (uc UseCase) CreateUser(user UserParam) (entities.User, error) {
+	var newUser *entities.User
+
+	newUser = &entities.User{
+		Name:      user.Name,
+		Email:     user.Email,
+		Password:  user.Password,
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	}
+
+	_, err := uc.userRepo.CreateUser(newUser)
+	if err != nil {
+		return *newUser, err
+	}
+	return *newUser, nil
 }
