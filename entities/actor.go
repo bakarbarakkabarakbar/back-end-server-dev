2035a44c6004d153ea2cfd3453273b654cdeccbc
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-03 19:55:37 +0700

    refactor: actor entity

--- entities/actor.go
+++ entities/actor.go
@@ -4,11 +4,11 @@ import "time"
 
 type Actor struct {
 	Id         uint   `gorm:"primary_key"`
-	Username   string `gorm:"column:user_id"`
-	Password   string `gorm:"column:token"`
+	Username   string `gorm:"column:username"`
+	Password   string `gorm:"column:password"`
 	RoleId     uint   `gorm:"column:role_id"`
-	IsVerified bool   `gorm:"column:is_verified"`
-	IsActive   bool   `gorm:"column:is_active"`
+	IsVerified string `gorm:"column:is_verified"`
+	IsActive   string `gorm:"column:is_active"`
 	CreatedAt  time.Time
 	ModifiedAt time.Time
 }
