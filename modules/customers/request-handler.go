39ffbdab9176f6fed1c834be6fb3e6a850971575
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-03 13:05:47 +0700

    fix: revamp customer request handler

--- modules/customers/request-handler.go
+++ modules/customers/request-handler.go
@@ -11,58 +11,47 @@ import (
 )
 
 type RequestHandler struct {
-	ctrl Controller
+	ctrl ControllerInterface
 }
 
 func NewRequestHandler(dbCrud *gorm.DB) RequestHandler {
 	return RequestHandler{
 		ctrl: Controller{
 			uc: UseCase{
-				userRepo: repositories.NewCustomerRepo(dbCrud)},
+				customerRepo: repositories.NewCustomerRepo(dbCrud)},
 		},
 	}
 }
 
 type RequestHandlerInterface interface {
-	GetUserByID(request dto.Request) dto.Response
+	GetCustomer(c *gin.Context)
 }
 
-func (rh RequestHandler) GetUsedById(c *gin.Context) {
-	var request = UserParam{}
-	var err = c.BindQuery(&request)
+func (rh RequestHandler) GetCustomer(c *gin.Context) {
+	var customerId uint64
+	var res ResponseParam
+	var err error
+	var customerEmail string
+	customerId, err = strconv.ParseUint(c.Param("id"), 10, 64)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
-		return
-	}
+		customerEmail = c.Param("email")
+		if customerEmail == "" {
+			c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
+			return
+		}
 
-	var userId uint64
-	userId, err = strconv.ParseUint(c.Param("id"), 10, 64)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
-		return
+		res, err = rh.ctrl.GetCustomerByEmail(&CustomerParam{Email: customerEmail})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, dto.DefaultErrorResponse())
+			return
+		}
+		c.JSON(http.StatusOK, res)
+	} else {
+		res, err = rh.ctrl.GetCustomerById(&CustomerParam{Id: uint(customerId)})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, dto.DefaultErrorResponse())
+			return
+		}
+		c.JSON(http.StatusOK, res)
 	}
-
-	var res FindUser
-	res, err = rh.ctrl.GetUserById(uint(userId))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.DefaultErrorResponse())
-		return
-	}
-	c.JSON(http.StatusOK, res)
-}
-
-func (rh RequestHandler) CreateUser(c *gin.Context) {
-	var request = UserParam{}
-	var err = c.Bind(&request)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
-		return
-	}
-	res, err := rh.ctrl.CreateUser(request)
-	if err != nil {
-
-		c.JSON(http.StatusInternalServerError, dto.DefaultErrorResponse())
-		return
-	}
-	c.JSON(http.StatusOK, res)
 }
