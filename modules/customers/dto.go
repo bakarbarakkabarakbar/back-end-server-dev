5598de1c599fc4dd7e84a054817c09153ba5a073
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-03 13:06:04 +0700

    fix: revamp customer data transfer object

--- modules/customers/dto.go
+++ modules/customers/dto.go
@@ -2,25 +2,17 @@ package customers
 
 import (
 	"github.com/dibimbing-satkom-indo/onion-architecture-go/dto"
-	"github.com/dibimbing-satkom-indo/onion-architecture-go/entities"
 )
 
-type Payload struct {
-	ID int
+type CustomerParam struct {
+	Id        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Avatar    string `json:"avatar"`
 }
 
-type UserParam struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type SuccessCreate struct {
+type ResponseParam struct {
 	dto.ResponseMeta
-	Data UserParam `json:"data"`
-}
-
-type FindUser struct {
-	dto.ResponseMeta
-	Data entities.Customer `json:"data"`
+	Data CustomerParam `json:"data"`
 }
