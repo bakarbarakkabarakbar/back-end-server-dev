9e117e7d96b31666d3e9ec4f6b1e40f1d2b0444a
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-05 20:42:47 +0700

    feat: add dto for fetch data

---
+++ function/data-api/dto.go
@@ -0,0 +1,17 @@
+package data_api
+
+type ResponseParam struct {
+	Page       int             `json:"id"`
+	PerPage    int             `json:"per_page"`
+	Total      int             `json:"total"`
+	TotalPages int             `json:"total_pages"`
+	Data       []CustomerParam `json:"data"`
+}
+
+type CustomerParam struct {
+	Id        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Avatar    string `json:"avatar"`
+}
