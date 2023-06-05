c3d1d6239f7ba89fc450915e78094d75275cedc5
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-05 23:34:18 +0700

    fix: check customer api to inject data

--- modules/admin/controller.go
+++ modules/admin/controller.go
@@ -1,6 +1,10 @@
 package admin
 
-import "github.com/dibimbing-satkom-indo/onion-architecture-go/dto"
+import (
+	"errors"
+	"github.com/dibimbing-satkom-indo/onion-architecture-go/dto"
+	data_api "github.com/dibimbing-satkom-indo/onion-architecture-go/function/data-api"
+)
 
 type Controller struct {
 	uc UseCaseInterface
@@ -10,17 +14,36 @@ type ControllerInterface interface {
 	GetCustomerById(req *CustomerParam) (ResponseParam, error)
 	GetCustomersByName(req *CustomerParam) (ResponseParam, error)
 	GetCustomersByEmail(req *CustomerParam) (ResponseParam, error)
+	GetAllCustomers(req *uint) (ResponseParam, error)
 	CreateCustomer(req *CustomerParam) (ResponseParam, error)
 	ModifyCustomer(req *CustomerParam) (ResponseParam, error)
 	RemoveCustomerById(req *CustomerParam) (ResponseParam, error)
 
 	GetAdminById(req *ActorParam) (ResponseParam, error)
+	GetAdminsByUsername(req *ActorParam) (ResponseParam, error)
+	GetAllAdmins(req *uint) (ResponseParam, error)
 	CreateAdmin(req *ActorParamWithPassword) (ResponseParam, error)
 	ModifyAdmin(req *ActorParamWithPassword) (ResponseParam, error)
 }
 
 func (ctrl Controller) GetCustomerById(req *CustomerParam) (ResponseParam, error) {
-	var customer, err = ctrl.uc.GetCustomerById(req)
+	var url = "https://reqres.in/api/users?page=2"
+	var results []data_api.CustomerParam
+	var err error
+	var customer CustomerParam
+
+	results, err = data_api.GetData(&url)
+	for _, result := range results {
+		_ = ctrl.uc.CreateCustomer(&CustomerParam{
+			Id:        result.Id,
+			FirstName: result.FirstName,
+			LastName:  result.LastName,
+			Email:     result.Email,
+			Avatar:    result.Avatar,
+		})
+	}
+
+	customer, err = ctrl.uc.GetCustomerById(req)
 	if err != nil {
 		return ResponseParam{
 			ResponseMeta: dto.ResponseMeta{
@@ -46,7 +69,23 @@ func (ctrl Controller) GetCustomerById(req *CustomerParam) (ResponseParam, error
 }
 
 func (ctrl Controller) GetCustomersByName(req *CustomerParam) (ResponseParam, error) {
-	var customers, err = ctrl.uc.GetCustomersByName(req)
+	var url = "https://reqres.in/api/users?page=2"
+	var results []data_api.CustomerParam
+	var err error
+	var customers []CustomerParam
+
+	results, err = data_api.GetData(&url)
+	for _, result := range results {
+		_ = ctrl.uc.CreateCustomer(&CustomerParam{
+			Id:        result.Id,
+			FirstName: result.FirstName,
+			LastName:  result.LastName,
+			Email:     result.Email,
+			Avatar:    result.Avatar,
+		})
+	}
+
+	customers, err = ctrl.uc.GetCustomersByName(req)
 	if err != nil {
 		return ResponseParam{
 			ResponseMeta: dto.ResponseMeta{
@@ -72,7 +111,23 @@ func (ctrl Controller) GetCustomersByName(req *CustomerParam) (ResponseParam, er
 }
 
 func (ctrl Controller) GetCustomersByEmail(req *CustomerParam) (ResponseParam, error) {
-	var customers, err = ctrl.uc.GetCustomersByEmail(req)
+	var url = "https://reqres.in/api/users?page=2"
+	var results []data_api.CustomerParam
+	var err error
+	var customers []CustomerParam
+
+	results, err = data_api.GetData(&url)
+	for _, result := range results {
+		_ = ctrl.uc.CreateCustomer(&CustomerParam{
+			Id:        result.Id,
+			FirstName: result.FirstName,
+			LastName:  result.LastName,
+			Email:     result.Email,
+			Avatar:    result.Avatar,
+		})
+	}
+
+	customers, err = ctrl.uc.GetCustomersByEmail(req)
 	if err != nil {
 		return ResponseParam{
 			ResponseMeta: dto.ResponseMeta{
@@ -89,6 +144,59 @@ func (ctrl Controller) GetCustomersByEmail(req *CustomerParam) (ResponseParam, e
 		ResponseMeta: dto.ResponseMeta{
 			Success:      true,
 			MessageTitle: "Success GetCustomersByEmail",
+			Message:      "Success",
+			ResponseTime: "",
+		},
+		Data: customers,
+	}
+	return res, nil
+}
+
+func (ctrl Controller) GetAllCustomers(req *uint) (ResponseParam, error) {
+	var url = "https://reqres.in/api/users?page=2"
+	var results []data_api.CustomerParam
+	var err error
+	var customers []CustomerParam
+
+	results, err = data_api.GetData(&url)
+	for _, result := range results {
+		_ = ctrl.uc.CreateCustomer(&CustomerParam{
+			Id:        result.Id,
+			FirstName: result.FirstName,
+			LastName:  result.LastName,
+			Email:     result.Email,
+			Avatar:    result.Avatar,
+		})
+	}
+
+	if *req < 1 {
+		return ResponseParam{
+			ResponseMeta: dto.ResponseMeta{
+				Success:      false,
+				MessageTitle: "Failed GetAllCustomers",
+				Message:      errors.New("page is not valid").Error(),
+				ResponseTime: "",
+			},
+			Data: nil,
+		}, errors.New("page is not valid")
+	}
+	customers, err = ctrl.uc.GetAllCustomers(req)
+	if err != nil {
+		return ResponseParam{
+			ResponseMeta: dto.ResponseMeta{
+				Success:      false,
+				MessageTitle: "Failed GetAllCustomers",
+				Message:      err.Error(),
+				ResponseTime: "",
+			},
+			Data: nil,
+		}, err
+	}
+
+	var res = ResponseParam{
+		ResponseMeta: dto.ResponseMeta{
+			Success:      true,
+			MessageTitle: "Success GetAllCustomers",
 			Message:      "Success",
 			ResponseTime: "",
 		},
@@ -193,6 +301,69 @@ func (ctrl Controller) GetAdminById(req *ActorParam) (ResponseParam, error) {
 			ResponseTime: "",
 		},
 		Data: admin,
+	}
+	return res, nil
+}
+
+func (ctrl Controller) GetAdminsByUsername(req *ActorParam) (ResponseParam, error) {
+	var admin, err = ctrl.uc.GetAdminsByUsername(req)
+	if err != nil {
+		return ResponseParam{
+			ResponseMeta: dto.ResponseMeta{
+				Success:      false,
+				MessageTitle: "Failed GetAdminsByUsername",
+				Message:      err.Error(),
+				ResponseTime: "",
+			},
+			Data: nil,
+		}, err
+	}
+
+	var res = ResponseParam{
+		ResponseMeta: dto.ResponseMeta{
+			Success:      true,
+			MessageTitle: "Success GetAdminsByUsername",
+			Message:      "Success",
+			ResponseTime: "",
+		},
+		Data: admin,
+	}
+	return res, nil
+}
+
+func (ctrl Controller) GetAllAdmins(req *uint) (ResponseParam, error) {
+	if *req < 1 {
+		return ResponseParam{
+			ResponseMeta: dto.ResponseMeta{
+				Success:      false,
+				MessageTitle: "Failed GetAllAdmins",
+				Message:      errors.New("page is not valid").Error(),
+				ResponseTime: "",
+			},
+			Data: nil,
+		}, errors.New("page is not valid")
+	}
+	var customers, err = ctrl.uc.GetAllAdmins(req)
+	if err != nil {
+		return ResponseParam{
+			ResponseMeta: dto.ResponseMeta{
+				Success:      false,
+				MessageTitle: "Failed GetAllAdmins",
+				Message:      err.Error(),
+				ResponseTime: "",
+			},
+			Data: nil,
+		}, err
+	}
+
+	var res = ResponseParam{
+		ResponseMeta: dto.ResponseMeta{
+			Success:      true,
+			MessageTitle: "Success GetAllAdmins",
+			Message:      "Success",
+			ResponseTime: "",
+		},
+		Data: customers,
 	}
 	return res, nil
 }
