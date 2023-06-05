89d084046a2d3df7627ed18d4c81a2c99207f776
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-05 20:44:59 +0700

    fix: add func on reqhandler customer

--- modules/customers/request-handler.go
+++ modules/customers/request-handler.go
@@ -28,30 +28,41 @@ type RequestHandlerInterface interface {
 }
 
 func (rh RequestHandler) GetCustomer(c *gin.Context) {
-	var customerId uint64
 	var res ResponseParam
 	var err error
-	var customerEmail string
-	customerId, err = strconv.ParseUint(c.Query("id"), 10, 64)
-	if err != nil {
-		customerEmail = c.Query("email")
-		if customerEmail == "" {
-			c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
-			return
-		}
+	var queryParam map[string][]string
 
-		res, err = rh.ctrl.GetCustomerByEmail(&CustomerParam{Email: customerEmail})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, dto.DefaultErrorResponse())
+	queryParam = c.Request.URL.Query()
+
+	for key, value := range queryParam {
+		switch key {
+		case "id":
+			var customerId uint64
+			customerId, err = strconv.ParseUint(value[0], 10, 64)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
+				return
+			}
+			res, err = rh.ctrl.GetCustomerById(&CustomerParam{Id: uint(customerId)})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, dto.DefaultErrorWithResponse(res.ResponseMeta))
+				return
+			}
+			c.JSON(http.StatusOK, res)
+			return
+		case "email":
+			if value[0] == "" {
+				c.JSON(http.StatusBadRequest, dto.DefaultBadRequestResponse())
+				return
+			}
+			var customerEmail = value[0]
+			res, err = rh.ctrl.GetCustomerByEmail(&CustomerParam{Email: customerEmail})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, dto.DefaultErrorWithResponse(res.ResponseMeta))
+				return
+			}
+			c.JSON(http.StatusOK, res)
 			return
 		}
-		c.JSON(http.StatusOK, res)
-	} else {
-		res, err = rh.ctrl.GetCustomerById(&CustomerParam{Id: uint(customerId)})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, dto.DefaultErrorResponse())
-			return
-		}
-		c.JSON(http.StatusOK, res)
 	}
 }
