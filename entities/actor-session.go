b1d6e3e4b0a72b21ef2432eaf747fa44a1a7f551
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-05 20:43:15 +0700

    refactor: naming inconsistency

--- entities/actor-session.go
+++ entities/actor-session.go
@@ -2,7 +2,7 @@ package entities
 
 import "time"
 
-type UserSession struct {
+type ActorSession struct {
 	Id        uint   `gorm:"primary_key"`
 	UserId    uint   `gorm:"column:user_id"`
 	Token     string `gorm:"column:token"`
