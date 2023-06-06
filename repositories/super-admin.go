2117cc2bde8de917c2b1d25be7633b54e42dbf36
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-06 23:53:47 +0700

    feat: add register and status admin func

--- repositories/super-admin.go
+++ repositories/super-admin.go
@@ -1,7 +1,7 @@
 package repositories
 
 import (
-	"github.com/dibimbing-satkom-indo/onion-architecture-go/entities"
+	"back-end-server-dev/entities"
 	"gorm.io/gorm"
 )
 
@@ -18,7 +18,14 @@ func NewSuperAdminRepo(dbCrud *gorm.DB) SuperAdminRepo {
 type SuperAdminRepoInterface interface {
 	GetVerifiedAdmins() ([]entities.Actor, error)
 	GetActiveAdmins() ([]entities.Actor, error)
+	GetRegisterAdminById(id *uint) (entities.RegisterApproval, error)
+	GetApprovedAdmins() ([]entities.RegisterApproval, error)
+	GetRejectedAdmins() ([]entities.RegisterApproval, error)
+	GetPendingAdmins() ([]entities.RegisterApproval, error)
+	GetRegisterRequestAdmins() ([]entities.RegisterApproval, error)
+	ModifyAdminApproval(adminRegister *entities.RegisterApproval) error
 	RemoveAdminById(id *uint) error
+	RemoveRegisterAdminById(id *uint) error
 }
 
 func (sar SuperAdminRepo) GetVerifiedAdmins() ([]entities.Actor, error) {
@@ -39,10 +46,71 @@ func (sar SuperAdminRepo) GetActiveAdmins() ([]entities.Actor, error) {
 	return result, nil
 }
 
+func (sar SuperAdminRepo) GetRegisterAdminById(id *uint) (entities.RegisterApproval, error) {
+	var register entities.RegisterApproval
+	var err = sar.db.First(&register, id).Error
+	if err != nil {
+		return register, err
+	}
+	return register, nil
+}
+
+func (sar SuperAdminRepo) GetApprovedAdmins() ([]entities.RegisterApproval, error) {
+	var result = make([]entities.RegisterApproval, 0)
+	var err = sar.db.Where("status = ?", "approved").Find(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (sar SuperAdminRepo) GetRejectedAdmins() ([]entities.RegisterApproval, error) {
+	var result = make([]entities.RegisterApproval, 0)
+	var err = sar.db.Where("status = ?", "rejected").Find(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (sar SuperAdminRepo) GetPendingAdmins() ([]entities.RegisterApproval, error) {
+	var result = make([]entities.RegisterApproval, 0)
+	var err = sar.db.Where("status = ?", "pending").Find(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (sar SuperAdminRepo) GetRegisterRequestAdmins() ([]entities.RegisterApproval, error) {
+	var result = make([]entities.RegisterApproval, 0)
+	var err = sar.db.Where("status = ?", "pending").Find(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (sar SuperAdminRepo) ModifyAdminApproval(adminRegister *entities.RegisterApproval) error {
+	err := sar.db.Save(&adminRegister).Error
+	return err
+}
+
 func (sar SuperAdminRepo) RemoveAdminById(id *uint) error {
 	var admin *entities.Actor
 	var err error
 	err = sar.db.Delete(&admin, id).Error
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sar SuperAdminRepo) RemoveRegisterAdminById(id *uint) error {
+	var register *entities.RegisterApproval
+	var err error
+	err = sar.db.Delete(&register, id).Error
 
 	if err != nil {
 		return err
