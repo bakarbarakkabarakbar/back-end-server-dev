09450d74858c40c2f35d004013157ddb53cc8dc1
bakarbarakkabarakbar <akbar.muhammadakbarmaulana@gmail.com>
2023-06-06 23:53:17 +0700

    feat: add session func

--- modules/auth/usecase.go
+++ modules/auth/usecase.go
@@ -1,6 +1,10 @@
 package auth
 
-import "github.com/dibimbing-satkom-indo/onion-architecture-go/repositories"
+import (
+	"back-end-server-dev/entities"
+	"back-end-server-dev/repositories"
+	"time"
+)
 
 type UseCase struct {
 	authRepo repositories.AuthRepo
@@ -8,13 +12,40 @@ type UseCase struct {
 
 type UseCaseInterface interface {
 	GetCredentialByUsername(account *CredentialParam) (CredentialParam, error)
+	CreateActorSession(customer *ActorSession) error
+	GetLastActorSessionByToken(account *ActorSession) (ActorSession, error)
 }
 
 func (uc UseCase) GetCredentialByUsername(account *CredentialParam) (CredentialParam, error) {
 	var result, err = uc.authRepo.GetActorByUsername(&account.username)
 	return CredentialParam{
+		id:       result.Id,
 		username: result.Username,
 		password: result.Password,
 		roleId:   result.RoleId,
 	}, err
+}
+
+func (uc UseCase) GetLastActorSessionByToken(account *ActorSession) (ActorSession, error) {
+	var result, err = uc.authRepo.GetLastActorSessionByToken(&account.Token)
+	return ActorSession{
+		Id:      result.Id,
+		ActorId: result.UserId,
+		Token:   result.Token,
+	}, err
+}
+
+func (uc UseCase) CreateActorSession(customer *ActorSession) error {
+	var newSession *entities.ActorSession
+
+	newSession = &entities.ActorSession{
+		Id:        customer.Id,
+		UserId:    customer.ActorId,
+		Token:     customer.Token,
+		CreatedAt: time.Now(),
+		ExpiresAt: time.Now().Add(time.Hour * 1),
+	}
+
+	var err = uc.authRepo.CreateActorSession(newSession)
+	return err
 }
